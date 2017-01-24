1ca4a46edd8406b5845ae6036ab51abb118900e9
jbok4 <jbok4@aol.com>
2017-01-24 17:42:11 -0500

    paragraph edit

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,7 +11,7 @@
               
               $scope.subtitle = 'Thank you for visiting the Championship Tracker!';
               
-              $scope.text = 'The idea is to be a wikipedia style site (where visitors can enter both new awards and teams) to build an encyclopedic reference of all your favorite teams achievements. Visit an existing team and read about or add a new title, and if your favorite team or the team youre trying to find out about isnt on the list, add it and watch as fellow visitors do some research and add some facts. The site is always growing.';
+              $scope.text = 'A wikipedia style site (where visitors can enter both new awards and teams) to build an encyclopedic reference of all your favorite teams achievements. Visit an existing team and read about or add a new title, and if your favorite team or the team youre trying to find out about isnt on the list, add it and watch as fellow visitors do some research and add some facts. The site is always growing.';
               
               $scope.pic = 'http://www.btlliners.com/wp-content/uploads/2016/02/sports-field-liners.jpg';
         });
